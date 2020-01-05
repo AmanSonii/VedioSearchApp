60fb9cb02177b95ebc43ccee2aa34b9739461084
amansoni887127@gmail.com <a.soni@eicare.org>
2020-01-05 10:45:02 +0530

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component
+{
+    render()
+    {
+        return (
+            <div>
+                This is app component
+            </div>
+        )
+    }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+//import React and ReactDOM library 
+//Make  react component
+// Take react component and show it on the screen 
+import React from 'react';
+import ReactDOM from 'react-dom'
+import App from './Components/App'
+
+
+ReactDOM.render(<App></App>,document.querySelector('#root'));
