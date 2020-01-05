6f1656db13decf9a290048acb40c052bcc091a5d
amansoni887127@gmail.com <a.soni@eicare.org>
2020-01-05 19:25:29 +0530

    app component changed

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,7 +6,7 @@ class App extends React.Component
     {
         return (
             <div>
-                This is app component
+                This is app
             </div>
         )
     }
